1c1206eeebb344419a9591c344c0c8c34a206c06
Caio Dutra <contato.caiodutra08@gmail.com>
2023-07-27 10:24:20 -0300

    Terceira aula/ Third Class

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro App</p>
-    </div>
-  );
+	const name = "Caio";
+	const newName = name.toUpperCase();
+	const sum = (a, b) => {
+		return a + b;
+	};
+	const url = "https://via.placeholder.com/150";
+
+	return (
+		<div className="App">
+			<h2>Alterando o JSX!</h2>
+			<p>Olá {newName}</p>
+			<p>Soma: {sum(1, 2)}</p>
+			<img src={url} alt="Minha Imagem" />
+		</div>
+	);
 }
 
 export default App;
