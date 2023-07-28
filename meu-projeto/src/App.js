1aa0bf2ec51717855f06ef35349467f919b9467e
Caio Dutra <contato.caiodutra08@gmail.com>
2023-07-28 14:16:42 -0300

    Quinta aula/ Fifth Class

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,21 +1,28 @@
 import "./App.css";
 import HelloWorld from "./components/HelloWorld";
+import SayMyName from "./components/SayMyName";
+import Pessoa from "./components/Pessoa";
 
 function App() {
-	const name = "Caio";
-	const newName = name.toUpperCase();
-	const sum = (a, b) => {
-		return a + b;
+	const nome = "John Doe";
+	const props = {
+		nome: "Caio",
+		idade: 19,
+		profissao: "Programador",
+		foto: "https://via.placeholder.com/150",
 	};
-	const url = "https://via.placeholder.com/150";
-
 	return (
 		<div className="App">
-			<h2>Alterando o JSX!</h2>
-			<p>Olá {newName}</p>
-			<p>Soma: {sum(1, 2)}</p>
-			<img src={url} alt="Minha Imagem" />
 			<HelloWorld />
+			<SayMyName nome="Caio" />
+			<SayMyName nome={nome} />
+			<Pessoa
+				nome="Caio"
+				idade="19"
+				profissao="Programador"
+				foto="https://via.placeholder.com/150"
+			/>
+			<Pessoa {...props} />
 		</div>
 	);
 }
