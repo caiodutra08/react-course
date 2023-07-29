54075464af21de473b1e4658e711db6f12fae026
Caio Dutra <contato.caiodutra08@gmail.com>
2023-07-29 17:12:08 -0300

    Oitava aula/ Eight class

--- meu-projeto/src/components/List.js
+++ meu-projeto/src/components/List.js
@@ -1,12 +1,13 @@
-import Item from "./Item"
+import Item from "./Item";
 
 function List() {
 	return (
 		<>
 			<h1>Minha Lista</h1>
 			<ul>
-				<Item marca="Ferrari" />
-				<Item marca="Fiat"/> 
+				<Item marca="Ferrari" ano_lancamento={1985} />
+				<Item marca="Fiat" ano_lancamento={1964} />
+				<Item />
 			</ul>
 		</>
 	);
