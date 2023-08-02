b75d97897df260c18e20b31d76d580f283f7056f
Caio Dutra <contato.caiodutra08@gmail.com>
2023-08-02 11:56:03 -0300

    Decima quarta aula/ Fourteenth class

---
+++ meu-projeto/src/components/Saudacao.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Saudacao({ nome }) {
+	function gerarSaudacao(algumNome) {
+		return `Olá ${algumNome}, tudo bem?`;
+	}
+	return <>{nome && <p>{gerarSaudacao(nome)}</p>}</>;
+}
+
+export default Saudacao;
