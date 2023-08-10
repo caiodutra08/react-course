0443582338e69021f21bfef1a7fdca3594e405e2
Caio Dutra <contato.caiodutra08@gmail.com>
2023-08-10 11:42:38 -0300

    Vigesima quinta aula/ Twenty-fifth class

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Message from "../layout/Message";
+import { useLocation } from "react-router-dom";
 
 function Projects() {
-  return (
-    <div>Projects</div>
-  )
+	const location = useLocation();
+	let message = "";
+	
+	if (location.state) {
+		message = location.state.message;
+	}
+
+	return (
+		<div>
+			<h1>Meus projetos</h1>
+			{message && <Message msg={message} type="success" />}
+		</div>
+	);
 }
 
-export default Projects
+export default Projects;
