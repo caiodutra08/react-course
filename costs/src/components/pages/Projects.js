e78922baf7fbcf971c218f434d5c8e6d29ede725
Caio Dutra <contato.caiodutra08@gmail.com>
2023-08-10 15:53:51 -0300

    Vigesima sexta aula/ Twenty-sixth class

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,19 +1,29 @@
 import React from "react";
 import Message from "../layout/Message";
 import { useLocation } from "react-router-dom";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton"
+
+import styles from "./Projects.module.css";
 
 function Projects() {
 	const location = useLocation();
 	let message = "";
-	
+
 	if (location.state) {
 		message = location.state.message;
 	}
 
 	return (
-		<div>
-			<h1>Meus projetos</h1>
+		<div className={styles.project_container}>
+			<div className={styles.title_container}>
+				<h1>Meus projetos</h1>
+				<LinkButton to="/newproject" text="Criar projeto"/>
+			</div>
 			{message && <Message msg={message} type="success" />}
+			<Container customClass="start">
+				<p>Projetos...</p>
+			</Container>
 		</div>
 	);
 }
